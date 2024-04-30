a8c35df40c23637f585e25494cac00f3072c2aee
amirullazmi0 <amirullazmi0@gmail.com>
2024-05-01 05:28:46 +0700

    env

--- prisma/migrations/20240430160343_new/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `proyekId` on the `kegiatans` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `kegiatans` DROP COLUMN `proyekId`;
--- prisma/migrations/20240430172540_new/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `durasi` on the `kegiatans` table. The data in that column could be lost. The data in that column will be cast from `VarChar(250)` to `Int`.
-
-*/
--- AlterTable
-ALTER TABLE `kegiatans` MODIFY `durasi` INTEGER NOT NULL;
